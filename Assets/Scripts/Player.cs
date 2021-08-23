9dd2a57bdf52e036ff68b1248f459993a19ee4f0
Alec <movieornasasim@gmail.com>
2021-08-23 18:39:28 -0500

    Created Basic Health System
    Added the ability reduce and regain health in the playercontroller script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(TankController))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    int _maxHealth;
+    int _currentHealth;
+    TankController _tankController;
+    private void Awake()
     {
-        
+        _tankController = GetComponent<TankController>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        _currentHealth = _maxHealth;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+    }
+
+    public void increaseHealth(int amount)
+    {
+        _currentHealth += amount;
+    }
+
+    public void decreaseHealth(int amount)
+    {
+        _currentHealth -= amount;
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+
     void Update()
     {
         
