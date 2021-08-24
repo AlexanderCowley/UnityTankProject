74a738f1dbf60d47854c6fe0c7480bd861ba0079
Alec <movieornasasim@gmail.com>
2021-08-24 15:37:09 -0500

    Added base collectible script and object.

---
+++ Assets/Scripts/Collectibles/CollectibleBase.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class CollectibleBase : MonoBehaviour
+{
+    protected abstract void Collect(Player player);
+    [SerializeField]
+    float _movementSpeed = 1f;
+
+    Rigidbody rigidBody;
+
+    [SerializeField]
+    ParticleSystem _collectedParticles;
+
+    [SerializeField]
+    AudioClip __collectedAudioClip;
+
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    protected virtual void Movement(Rigidbody rb)
+    {
+        Quaternion turnOffset = Quaternion.Euler(0f, _movementSpeed, 0f);
+        rb.MoveRotation(rb.rotation * turnOffset);
+    }
+
+    void Feedback()
+    {
+        if(_collectedParticles != null)
+        {
+            Instantiate(_collectedParticles, transform.position, Quaternion.identity);
+        }
+
+        if (__collectedAudioClip != null)
+        {
+            AudioHelper.PlayClip2D(__collectedAudioClip, 1f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<Player>();
+        if(player != null)
+        {
+            Collect(player);
+            Feedback();
+
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Movement(rigidBody);
+    }
+}
