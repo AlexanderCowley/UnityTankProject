adb8b3df96361c22d2f521e2896702de24a73eb0
Alec <movieornasasim@gmail.com>
2021-09-15 14:44:48 -0500

    Created a patrol and chase movement pattern for the boss enemy
    Boss enemy can patrol back and forth when the player is out of his sight.
    When the player enters their field of view, they chase them.

--- Assets/Scripts/Enemy/Boss.cs
+++ Assets/Scripts/Enemy/Boss.cs
@@ -1,13 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 public class Boss : Enemy
 {
-
     bool _isPlayerDetected = false;
     [SerializeField] float _movementSpeed;
 
-    float normalizedTime;
-    float directionChangeTime = 3f;
+    [SerializeField] TankController player;
+
+    float distanceToTarget = 3f;
+    Vector3 startingPos;
     public bool IsPlayerDetected
     {
         set => _isPlayerDetected = value;
@@ -15,47 +17,31 @@ public class Boss : Enemy
 
     void Awake()
     {
-        
+        startingPos = transform.position;
     }
 
     public override void Move()
     {
         if (!_isPlayerDetected)
-            GoToTarget();
+            MoveBackAndForth();
         else
             TargetPlayer();
     }
 
-    void GoToTarget()
+    //Patrol
+    void MoveBackAndForth()
     {
-        print("go to target");
-        Vector3 currentTarget = FindDirection();
-        float distance = Vector3.Distance(currentTarget, transform.position);
-        print(distance);
-
-        while(distance > 10f)
-        {
-            transform.position = Vector3.Lerp
-                (transform.position, currentTarget, _movementSpeed * Time.deltaTime);
-        }
-        
+        Vector3 velocity = startingPos;
+        velocity.x += distanceToTarget * Mathf.Sin(Time.time * _movementSpeed);
+        transform.position = velocity;
     }
 
-    Vector3 FindDirection()
-    {
-        Vector3 randomDir = 
-            new Vector3(transform.position.x + Random.Range(-1, 1f), 
-            transform.position.y,
-            transform.position.z + Random.Range(-1, 1f));
-        randomDir *= 3f;
-        randomDir.y = transform.position.y;
-
-        return randomDir;
-    }
-
+    //Chase Target and method to decide attacks
     void TargetPlayer()
     {
         print("huh");
+        transform.position = Vector3.MoveTowards
+            (transform.position, player.transform.position, _movementSpeed * Time.deltaTime);
     }
 
     /*bool InPlayerRange()
@@ -71,8 +57,6 @@ public class Boss : Enemy
         //else
             //ShootProjectile
     }
-
-    //Move between them
 
     //Player In Range of Melee Attack
 
