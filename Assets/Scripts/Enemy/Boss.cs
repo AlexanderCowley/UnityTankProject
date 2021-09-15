f10bbc46233c156a075246dfd6e196f7f8e4184e
Alec <movieornasasim@gmail.com>
2021-09-15 15:24:34 -0500

    Created a charge attack for the boss enemy
    The boss enemy can now charge the enemy by increasing in speed when they are in a certain range. Bumping into the player drains there health.

--- Assets/Scripts/Enemy/Boss.cs
+++ Assets/Scripts/Enemy/Boss.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 public class Boss : Enemy
 {
     bool _isPlayerDetected = false;
-    [SerializeField] float _movementSpeed;
+    float _movementSpeed;
+    float _defaultSpeed = .75f;
+    float _chargingSpeed = 1.4f;
+
+    bool _isAttacking = false;
 
     [SerializeField] TankController player;
 
@@ -18,6 +22,7 @@ public class Boss : Enemy
     void Awake()
     {
         startingPos = transform.position;
+        _movementSpeed = _defaultSpeed;
     }
 
     public override void Move()
@@ -42,25 +47,63 @@ public class Boss : Enemy
         print("huh");
         transform.position = Vector3.MoveTowards
             (transform.position, player.transform.position, _movementSpeed * Time.deltaTime);
+
+        AttackPlayer();
     }
 
-    /*bool InPlayerRange()
+    bool InPlayerRange()
     {
-
-        return 
-    }*/
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        return distanceToPlayer <= 3f;
+    }
 
     void AttackPlayer()
     {
-        //if(InPlayerRange())
-            //Charge Player
-        //else
-            //ShootProjectile
+        if (InPlayerRange())
+            Charge();
+        else
+            ShootProjectile();
     }
 
-    //Player In Range of Melee Attack
+    void Charge()
+    {
+        if (!_isAttacking)
+        {
+            _isAttacking = true;
+
+            StartCoroutine(attackDelay(.5f));
+            _movementSpeed = _chargingSpeed;
+            //fx
+            StartCoroutine(attackDelay(2f));
+            _movementSpeed = _defaultSpeed;
+            _isAttacking = false;
+            //fx = power down
+        }
+    }
+
+    void ShootProjectile()
+    {
+        print("wha");
+        if (!_isAttacking)
+        {
+            _isAttacking = true;
+
+            StartCoroutine(attackDelay(.5f));
+            // things
+            //fx
+            StartCoroutine(attackDelay(2f));
+            //end things
+            _isAttacking = false;
+            //fx = power down
+        }
+    }
+
+    IEnumerator attackDelay(float delayTimer)
+    {
+        yield return new WaitForSeconds(delayTimer);
+        print("finished");
+        yield return null;
+    }
 
     //Projectile Attack
-
-    //Charge Attack
 }
